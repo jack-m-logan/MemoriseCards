5061327860015334bfaa59521fcf212bdd1ef01d
Jack Logan <jackmlogan92@gmail.com>
2023-07-01 17:10:59 +0100

    add card matrix button dropdown

--- MemoriseCards/MemoriseCards/wwwroot/js/site.js
+++ MemoriseCards/MemoriseCards/wwwroot/js/site.js
@@ -1,7 +1,4 @@
-﻿const aboutBtn = $("#aboutBtn");
-const titleBanner = $("#titleBanner");
-
-$(document).ready(function () {
+﻿$(document).ready(function () {
     const aboutDialog = $("#aboutDialog");
     aboutDialog.removeClass("hidden");
 });
@@ -9,21 +6,33 @@ $(document).ready(function () {
 $("#closeAboutBtn").click(() => {
     $("#aboutDialog").addClass("hidden");
     $("#overlay").removeClass("fixed");
-    aboutBtn.removeClass("bg-zinc-500");
-    titleBanner.removeClass("hidden");
+    $("#aboutBtn").removeClass("bg-zinc-600");
+    $("#titleBanner").removeClass("hidden");
 });
 
 $("#aboutBtn").click(() => {
     $("#aboutDialog").removeClass("hidden");
     $("#overlay").toggleClass("fixed");
-    aboutBtn.toggleClass("bg-zinc-500");
-    titleBanner.addClass("hidden");
-});
-
-$("#matrixBtn").click(() => {
-    $("#matrixBtn").toggleClass("bg-zinc-500");
+    $("#aboutBtn").toggleClass("bg-zinc-600 hover:bg-zinc-400");
+    $("#titleBanner").addClass("hidden");
 });
 
 $("#practiceBtn").click(() => {
-    $("#practiceBtn").toggleClass("bg-zinc-500");
+    $("#practiceBtn").toggleClass("bg-zinc-600 hover:bg-zinc-400");
+});
+
+$("#matrixBtn").click(() => {
+    $("#matrixDropdown").toggleClass("hidden");
+    $("#matrixBtn").toggleClass("bg-zinc-600 hover:bg-zinc-400");
+});
+
+$(document).click((e) => {
+    if (!$(e.target).closest("#matrixBtn").length) {
+        $("#matrixDropdown").addClass("hidden");
+        $("#matrixBtn").removeClass("bg-zinc-600").addClass("hover:bg-zinc-400");
+    }
+});
+
+$("#newDeckBtn, #editDeckBtn").click(() => {
+    $("#newDeckBtn, #editDeckBtn").toggleClass("bg-zinc-600 hover:bg-zinc-400");
 });
